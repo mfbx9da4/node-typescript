0b98e895eaf6dfd900cbe5fe8be9d3e4d197f091
David Adler <dalberto.adler@gmail.com>
2020-02-04 18:10:31 +0000

    parse input

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
-import { runTestAndWriteData, generateSorted, assert } from './utils'
+import fs from 'fs'
 
-// const array = [0, 8, 17, 35, 49, 52, 59, 76]
-
-function indexOfSorted(num, array) {
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-    if (element === num) {
-      return i
-    }
-  }
-  return -1
+function solve(lines: string[]) {
+  console.log('lines', lines)
 }
 
 function main() {
-  runTestAndWriteData(indexOfSorted)
-  const array = [6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]
-  assert(array.indexOf(10) === indexOfSorted(10, array))
-  assert(array.indexOf(13) === indexOfSorted(13, array))
-  assert(array.indexOf(96) === indexOfSorted(96, array))
+  const stdinBuffer = fs.readFileSync(0)
+  const lines = stdinBuffer
+    .toString()
+    .split('\n')
+    .filter((x) => x !== '')
+  solve(lines)
 }
 
 main()
--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,32 @@ import perf from 'perf_hooks'
 import { writeData } from './writeData'
 import { indexOfSortedRef } from './indexOfSorted'
 
+// function indexOfSorted(array, num) {
+//   for (let i = 0; i < array.length; i++) {
+//     const value = array[i]
+//     if (value === num) {
+//       return i
+//     }
+//   }
+//   return -1
+// }
+
+// const array = [0, 8, 17, 35, 49, 52, 59, 76]
+
+export function indexOf(num, array) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i]
+    if (element === num) {
+      return i
+    }
+  }
+  return -1
+}
+
+// assert(array.indexOf(10) === indexOfSorted(10, array))
+// assert(array.indexOf(13) === indexOfSorted(13, array))
+// assert(array.indexOf(96) === indexOfSorted(96, array))
+
 export function assert(bool) {
   if (bool) {
     console.log('✅')
@@ -32,50 +58,61 @@ export function generateSorted(num: number): Array<number> {
   return out
 }
 
+function average(arr: Array<number>): number {
+  return arr.reduce((prev, cur) => prev + cur, 0) / arr.length
+}
+
 export function runTest(fn, includeRef = false) {
+  let includeNative = true
   const labels = []
   const IndexOf = []
-  const IndexOfSorted = []
+  const ours = []
   const IndexOfSortedRef = []
   let arraySize = 10000
   let endSize = Math.pow(10, 7)
   let increment = (endSize - arraySize) / 8
+  let testN = 7
   for (let i = 0; i < 8; i++) {
-    // Create array
+    // Create test env
     var num = Math.random() * 100
-    // arraySize = 10 * i
-    // arraySize = Math.pow(10, i)
     arraySize += increment
     labels.push(`${Math.floor(arraySize / 1000000)}m numbers`)
-    var testArray = generateSorted(arraySize)
+    var ref = []
+    var _ours = []
+    var _indexOf = []
 
-    // time them
-    if (includeRef) {
-      var _indexOfSortedRef = time(() => indexOfSortedRef(num, testArray))
-      IndexOfSortedRef.push(_indexOfSortedRef)
+    // Do tests
+    for (let j = 0; j < testN; j++) {
+      // Create array
+      var testArray = generateSorted(arraySize)
+      // time them
+      _ours.push(time(() => fn(num, testArray)))
+      if (includeRef) ref.push(time(() => indexOfSortedRef(num, testArray)))
+      if (includeNative) _indexOf.push(time(() => testArray.indexOf(num)))
     }
-    var _indexOfSorted = time(() => fn(num, testArray))
-    IndexOfSorted.push(_indexOfSorted)
-    var _indexOf = time(() => testArray.indexOf(num))
-    IndexOf.push(_indexOf)
+
+    IndexOfSortedRef.push(average(ref))
+    ours.push(average(_ours))
+    IndexOf.push(average(_indexOf))
 
     // Log
     console.log('Size of array', arraySize)
     console.log(_indexOf)
-    console.log(_indexOfSorted)
+    console.log(ours)
   }
 
   const out = {
     labels,
-    IndexOf,
-    IndexOfSorted,
   }
-  if (includeRef) out['IndexOfSortedRef'] = IndexOfSortedRef
+  if (includeRef) out['💪 Binary Search'] = IndexOfSortedRef
+  if (includeNative) out['Native (indexOf)'] = IndexOf
+  // out['Linear (indexOf)'] = ours
 
   return out
 }
 
-export function runTestAndWriteData(fn, includeRef = false) {
+type NewFuncType = (target: number, array: Array<number>) => number
+export function runTestAndWriteData(fn: NewFuncType, includeRef = false) {
   const data = runTest(fn, includeRef)
   writeData(data)
   return data
