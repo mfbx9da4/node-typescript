e243130f86371261f5052ae478b83a789a4da1ae
David Adler <dalberto.adler@gmail.com>
2020-02-05 13:59:45 +0000

    codeforces coin distribution polycarb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
 import fs from 'fs'
 
+function asArrayNumber(testCase: string): number[] {
+  return testCase.split(' ').map((x) => {
+    return parseInt(x, 10)
+  })
+}
+
+function findGreatest(sisters: Array<number>): number {
+  return Math.max(...sisters)
+}
+
+function calculateDifference(greatest: number, sisters: number[]): number {
+  let difference = 0
+  for (let i = 0; i < sisters.length; i++) {
+    difference = greatest - sisters[i] + difference
+  }
+  return difference
+}
+
 function solve(lines: string[]) {
-  console.log('lines', lines)
+  const testCases = lines.slice(1)
+  for (let i = 0; i < testCases.length; i++) {
+    const numbers = asArrayNumber(testCases[i])
+    const sisters = [numbers[0], numbers[1], numbers[2]]
+    const n = numbers[3]
+    const greatest = findGreatest(sisters)
+    const difference = calculateDifference(greatest, sisters)
+    const remainder = n - difference
+    if (remainder >= 0 && remainder % 3 === 0) {
+      console.log('YES')
+    } else {
+      console.log('NO')
+    }
+  }
 }
 
 function main() {
